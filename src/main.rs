0805be8163cb3baca7cfed4fa3019923ce9ad477
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-06-03 16:16:14 -0500

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate clap;
 
 use clap::{App, AppSettings, Arg, SubCommand};
-use crates_io_api::{SyncClient, Error as CrateError};
+use crates_io_api::{Error as CrateError, SyncClient};
 
 fn main() {
     let matches = App::new("cargo-crates")
@@ -22,7 +22,7 @@ fn main() {
         )
         .settings(&[AppSettings::SubcommandRequired])
         .get_matches();
-    
+
     let sub_matches = matches.subcommand_matches("crates").unwrap();
 
     let crate_name = sub_matches.value_of("CRATE").unwrap();
@@ -30,18 +30,30 @@ fn main() {
 
     // Check if the crate name follows crates.io's regulations (excluding reserved names for windows)
     let under_max_length = crate_name.chars().take(65).count() <= 64;
-    let first_alphabetic = crate_name.chars().next().map(char::is_alphabetic).unwrap_or(false);
+    let first_alphabetic = crate_name
+        .chars()
+        .next()
+        .map(char::is_alphabetic)
+        .unwrap_or(false);
     let not_empty = !crate_name.is_empty();
-    let all_ascii_alphanum = crate_name.chars().all(|c| c.is_ascii_alphanumeric() || c == '_' || c == '-');
+    let all_ascii_alphanum = crate_name
+        .chars()
+        .all(|c| c.is_ascii_alphanumeric() || c == '_' || c == '-');
     if !(not_empty && all_ascii_alphanum && first_alphabetic && under_max_length) {
-        eprintln!("error: crate name '{}' is not valid for crates.io", crate_name);
+        eprintln!(
+            "error: crate name '{}' is not valid for crates.io",
+            crate_name
+        );
         std::process::exit(-1);
     }
 
     let exists = match check_crate_exists(crate_name) {
         Ok(e) => e,
         Err(e) => {
-            eprintln!("error: failed to lookup information for crate '{}': {}", crate_name, e);
+            eprintln!(
+                "error: failed to lookup information for crate '{}': {}",
+                crate_name, e
+            );
             std::process::exit(-1);
         }
     };
@@ -56,7 +68,7 @@ fn main() {
             Err(e) => {
                 eprintln!("error: failed to open link: {}", e);
                 std::process::exit(-1);
-            },
+            }
             Ok(()) => {}
         }
     } else {
@@ -64,7 +76,7 @@ fn main() {
             Err(e) => {
                 eprintln!("error: failed to open link: {}", e);
                 std::process::exit(-1);
-            },
+            }
             Ok(()) => {}
         }
     }
@@ -75,15 +87,10 @@ fn check_crate_exists(name: &str) -> Result<bool, CrateError> {
         "cargo-crates (github.com/ThePuzzlemaker/cargo-crates)",
         std::time::Duration::from_millis(2000),
     )?;
-    
-    match client.get_crate(name) {
-        Err(CrateError::NotFound(_)) => {
-            Ok(false)
-        },
-        Err(e) => {
-            Err(e)
-        },
-        _ => Ok(true)
 
+    match client.get_crate(name) {
+        Err(CrateError::NotFound(_)) => Ok(false),
+        Err(e) => Err(e),
+        _ => Ok(true),
     }
 }
